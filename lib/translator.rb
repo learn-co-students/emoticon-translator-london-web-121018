aa4eba99678900d76c2d29bc31f8fd4d8dc57cbc
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-12-04 11:24:44 +0000

    Bi-moticonal

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,60 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+{"angel"=>["O:)", "☜(⌒▽⌒)☞"],
+ "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+ "bored"=>[":O", "(ΘεΘ;)"],
+ "confused"=>["%)", "(゜.゜)"],
+ "embarrased"=>[":$", "(#^.^#)"],
+ "fish"=>["><>", ">゜))))彡"],
+ "glasses"=>["8D", "(^0_0^)"],
+ "grinning"=>["=D", "(￣ー￣)"],
+ "happy"=>[":)", "(＾ｖ＾)"],
+ "kiss"=>[":*", "(*^3^)/~☆"],
+ "sad"=>[":'(", "(Ｔ▽Ｔ)"],
+ "surprised"=>[":o", "o_O"],
+ "wink"=>[";)", "(^_-)"]}
+
+def load_library (file_path)
+  emoticons = YAML.load_file(file_path)
+  translator = {}
+  emoticons.each {|word, meanings|
+  meanings.each_with_index {|emoticon, index|
+    if index == 1 && translator["get_meaning"]
+    translator["get_meaning"][emoticon] = word
+  elsif index == 1
+    translator["get_meaning"] = {emoticon => word}
+  elsif index == 0 && translator["get_emoticon"]
+    translator["get_emoticon"][emoticon] = meanings[1]
+  elsif index == 0
+    translator["get_emoticon"] = {emoticon => meanings[1]}
+  end
+    }}
+translator
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file_path, emoticon)
+translator = load_library (file_path)
+translator.each {|get, translations|
+translations.each {|english, japanese|
+  if emoticon == english
+    return japanese
+  end
+  }
+}
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (file_path, emoticon)
+  translator = load_library (file_path)
+  translator.each {|get, translations|
+    if get == "get_meaning"
+  translations.each {|japanese, meaning|
+    if emoticon == japanese
+      return meaning    
+    end
+  }
+end
+}
+  return "Sorry, that emoticon was not found"
 end
